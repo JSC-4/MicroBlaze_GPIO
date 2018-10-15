6562d5514aa58d8096d8f3827c50b1addcb1ce80
JSCBLOG <J.samuelsclarke@Gmail.com>
2018-10-15 16:14:39 +0100

    finished project

---
+++ GPIO_uBlaze_N4/GPIO_uBlaze_N4/GPIO_uBlaze_N4.sdk/GPIO_Interface/Debug/src/GPIO_Test.d
@@ -0,0 +1,45 @@
+src/GPIO_Test.o src/GPIO_Test.o: ../src/GPIO_Test.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xil_printf.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xil_types.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xparameters.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/bspconfig.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xgpio.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xil_assert.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xstatus.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xgpio_l.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xil_io.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xil_printf.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/mb_interface.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xil_exception.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xparameters.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xil_printf.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xil_types.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xparameters.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/bspconfig.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xgpio.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xil_assert.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xstatus.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xgpio_l.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xil_io.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xil_printf.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/mb_interface.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xil_exception.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xparameters.h:
---
+++ GPIO_uBlaze_N4/GPIO_uBlaze_N4/GPIO_uBlaze_N4.sdk/GPIO_Interface/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xparameters.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xil_cache.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/mb_interface.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xil_types.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xil_assert.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xil_exception.h \
+ ../../GPIO_Interface_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../GPIO_Interface_bsp/microblaze_0/include/xparameters.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xil_cache.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/mb_interface.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xil_types.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xil_assert.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xil_exception.h:
+
+../../GPIO_Interface_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
